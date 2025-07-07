157fbb8448585fb364cfa666b3c0b91c2a690736
Shaym <48408387+0xShaym@users.noreply.github.com>
2025-07-07 22:22:57 +0200

    Update Moonlit Drifting.js add melo control

--- Moonlit Drifting.js
+++ Moonlit Drifting.js
@@ -1,16 +1,15 @@
 // @by Shaym
 // @title Moonlit Drifting
-
 setCps(140/60/4)
 
 const bass = "gm_synth_bass_1:8"
 const melo = "gm_synth_brass_2"
+
 
 $: stack(
   note("[d1(3, 8) f1(3, 8) e1(4, 8, 2) a1(3, 8)]/2").off(0.25, x=>x.add(7)).sound(bass).room(0.5).lpf("200 8000".slow(2)).pan("0.4 0.6").gain(1.2),
   note("[d1(3, 8) f1(3, 8) e1(4, 8, 2) a1(3, 8)]/2").off(0.125, x=>x.add(12)).sound(bass).room(0.5).lpf("8000 200".slow(2)).pan("0.4 0.6").gain(1.2),
   sound("[bd*4]").bank("rhodespolaris").gain(2),
   sound("[hh*8?, [- cp]*2]").bank("tr808"),
-  note("<e c [b f]>, b a b a").sound(melo).gain(1.5),
-  note("c < - [ c a c a]>").sound(melo).gain(2)
+  note("<e c [b f]>, b a b a, c < - [ c a c a]>").sound(melo).gain(1.5).degradeBy(slider(0, 0, 1)),
 ).pianoroll()
